8525d95feaeb58dbd81c751d0547c877cec6e84a
arifur rahman <arifurrex@gmail.com>
2024-09-10 21:37:52 +0600

    academic-year.blade.php file setup

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AcademicYearController;
 use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
@@ -20,6 +21,7 @@ Route::group(['prefix'=>'admin'],function(){
         Route::get('dashboard',[AdminController::class,'dashboard'])->name('admin.dashboard');
         Route::get('form',[AdminController::class,'form'])->name('admin.form');
         Route::get('table',[AdminController::class,'table'])->name('admin.table');
+        Route::get('academic-year/create',[AcademicYearController::class,'index'])->name('academic-year/create');
     });  
 });
 
